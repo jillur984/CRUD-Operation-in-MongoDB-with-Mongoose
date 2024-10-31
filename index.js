47781b504456f8e30c10fc1e270186cbe76f04e2
Md. Jillur Rahman <105236203+jillur984@users.noreply.github.com>
2024-10-31 16:53:04 +0600

    Connect DB by Mongoose

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const PORT = 3002;
+
+// connet mongoDB by Async Function
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://jillurr498:hm9Cuun2AjczCu48@jillur-db.7dqzy.mongodb.net/testProductDB"
+    );
+
+    console.log("DB is connected");
+  } catch (error) {
+    console.log("DB is not conneted");
+    console.log(error.message);
+    process.exit();
+  }
+};
+
+// this method mongoDB connect by then & catch
+// mongoose.connect("mongodb+srv://jillurr498:hm9Cuun2AjczCu48@jillur-db.7dqzy.mongodb.net/testProductDB")
+// .then(()=>console.log("DB is Connected"))
+// .catch((err)=>{
+//     console.log("DB is not connected"),
+//     console.log(err)
+//     process.exit(1)
+// })
+app.get("/", (req, res) => {
+  res.send("Hello I am From Server");
+});
+
+app.listen(PORT, async(req, res) => {
+  console.log(`Server is Runnig at http://localhost:${PORT}`);
+ await connectDB();
+});
