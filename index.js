fb6d1d21e93d3b86a942da9ff9a2e3a44e67e9d0
Md. Jillur Rahman <105236203+jillur984@users.noreply.github.com>
2024-10-31 21:20:57 +0600

    product add one or and many in MongoDB

--- index.js
+++ index.js
@@ -6,19 +6,29 @@ const app = express();
 const PORT = 3002;
 
 // create Products Schema by Mongoose
-const productsSchema=new mongoose.Schema({
-  title:String,
-  price:Number,
-  description:String,
-  createdAt:{
-    type:Date,
-    default:Date.now()
-  }
-})
+const productsSchema = new mongoose.Schema({
+  title: {
+    type:String,
+    required:true
+  },
+  price: {
+    type:Number,
+    required:true
+  },
+  description: {
+    type:String,
+    required:true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
 
-const product=mongoose.model("products",productsSchema)
+const Product = mongoose.model("products", productsSchema);
 
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // connet mongoDB by Async Function
 
@@ -48,7 +58,52 @@ app.get("/", (req, res) => {
   res.send("Hello I am From Server");
 });
 
-app.listen(PORT, async(req, res) => {
+app.post("/products", async(req, res) => {
+  try {
+    const title = req.body.title;
+    const price = req.body.price;
+    const description = req.body.description;
+
+    // when insert many that time no need new Data
+    const newData = new Product({
+      title: title,
+      price: price,
+      description: description,
+    });
+
+    // const productData =await newData.save();
+
+    // if i want to add product many that is ?
+
+    const productData=await Product.insertMany([
+      {
+        title:"IPHONE 5",
+        price:1450,
+        description:"This is Famous Phone"
+      },
+      {
+        title:"IPHONE 7",
+        price:1550,
+        description:"This is Famous Phone ever world"
+      }
+      , 
+      {
+        title:"IPHONE 9",
+        price:1850,
+        description:"This is Famous Phone and i like it"
+      }
+    ])
+
+    res.status(201).send(productData);
+
+  } catch (error) {
+    res.status(500).send({
+      message: error.message,
+    });
+  }
+});
+
+app.listen(PORT, async (req, res) => {
   console.log(`Server is Runnig at http://localhost:${PORT}`);
- await connectDB();
+  await connectDB();
 });
