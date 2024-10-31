eb21602d86687791111b4218df266ff59945939c
Md. Jillur Rahman <105236203+jillur984@users.noreply.github.com>
2024-10-31 22:56:21 +0600

    get all products_find all products_find one product

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ const PORT = 3002;
 // create Products Schema by Mongoose
 const productsSchema = new mongoose.Schema({
   title: {
-    type:String,
-    required:true
+    type: String,
+    required: true,
   },
   price: {
-    type:Number,
-    required:true
+    type: Number,
+    required: true,
   },
   description: {
-    type:String,
-    required:true
+    type: String,
+    required: true,
   },
   createdAt: {
     type: Date,
@@ -46,56 +46,27 @@ const connectDB = async () => {
   }
 };
 
-// this method mongoDB connect by then & catch
-// mongoose.connect("mongodb+srv://jillurr498:hm9Cuun2AjczCu48@jillur-db.7dqzy.mongodb.net/testProductDB")
-// .then(()=>console.log("DB is Connected"))
-// .catch((err)=>{
-//     console.log("DB is not connected"),
-//     console.log(err)
-//     process.exit(1)
-// })
 app.get("/", (req, res) => {
   res.send("Hello I am From Server");
 });
 
-app.post("/products", async(req, res) => {
+//creat new Products
+
+app.post("/products", async (req, res) => {
   try {
     const title = req.body.title;
     const price = req.body.price;
     const description = req.body.description;
 
-    // when insert many that time no need new Data
     const newData = new Product({
       title: title,
       price: price,
       description: description,
     });
 
-    // const productData =await newData.save();
-
-    // if i want to add product many that is ?
-
-    const productData=await Product.insertMany([
-      {
-        title:"IPHONE 5",
-        price:1450,
-        description:"This is Famous Phone"
-      },
-      {
-        title:"IPHONE 7",
-        price:1550,
-        description:"This is Famous Phone ever world"
-      }
-      , 
-      {
-        title:"IPHONE 9",
-        price:1850,
-        description:"This is Famous Phone and i like it"
-      }
-    ])
+    const productData = await newData.save();
 
     res.status(201).send(productData);
-
   } catch (error) {
     res.status(500).send({
       message: error.message,
@@ -103,7 +74,72 @@ app.post("/products", async(req, res) => {
   }
 });
 
+// get new products
+
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find();
+    // if i want only 2 data
+    // const products=await Product.find().limit(2)
+    if (products) {
+      res.status(200).send({
+        success: true,
+        message: "Return all products",
+        data: products,
+      });
+    } else {
+      res.status(404).send({
+        success: false,
+        message: "Product not found",
+      });
+    }
+  } catch (error) {
+    res.status(500).send({
+      message: error.message,
+    });
+  }
+});
+
+// search a product by ID
+
+app.get("/products/:id", async (req, res) => {
+  const id = req.params.id;
+
+  // find return a array but find one return a object
+  //  const products=await Product.find({_id:id}) // return a array
+  //  const products=await Product.findOne({_id:id}) // return a object
+
+  // if i want to show specific item
+  const product = await Product.find({ _id: id }).select({
+    title: 1,
+    _id: 0,
+    price: 1,
+  });
+  if (product) {
+    try {
+      res.status(200).send({
+        success: true,
+        message: "Return a single Product",
+        data: product,
+      });
+    } catch (error) {
+      res.status(404).send({
+        success: false,
+        message: "Product not found",
+      });
+    }
+  }
+});
+
 app.listen(PORT, async (req, res) => {
   console.log(`Server is Runnig at http://localhost:${PORT}`);
   await connectDB();
 });
+
+// database---collection---documents
+
+//GET:/products---Return all products
+//GET:/products/:id---Return a specific product
+//POST:/products---create a product
+//PUT:/products/:id---update a product based on id
+//DELETE:/products/:id-delete a product based on id.
